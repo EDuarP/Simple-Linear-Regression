cca07683646af2ef473fd99869ffa76012656bd6
Eduardo Duarte <111079136+EDuarP@users.noreply.github.com>
2022-09-14 13:57:27 -0500

    Add files via upload

--- Simple-Linear-Regression.py
+++ Simple-Linear-Regression.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
 x = np.array([5,15,25,35,45,55,65,75]).reshape((-1, 1))
@@ -15,3 +16,8 @@ x_new = np.random.randint(10, size=(10, 1))
 y_new = model.predict(x_new)
 print('New values:', x_new)
 print('predicted response:', y_new)
+plt.scatter(x_new, y_new, color='orange')
+plt.plot(x_new,model.coef_*x_new+model.intercept_, '-r')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
